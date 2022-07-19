eebd185a931acb1d14c967ac369aaae16bc64b9a
val-mx <54263914+val-mx@users.noreply.github.com>
2022-07-19 09:43:27 +0200

    Initial commit

---
+++ app/src/main/java/leko/valmx/uhrenprojekt/connection/BluetoothDiscoveryAdapter.kt
@@ -0,0 +1,34 @@
+package leko.valmx.uhrenprojekt.connection
+
+import android.bluetooth.BluetoothAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_bluetooth_device.view.*
+import leko.valmx.uhrenprojekt.R
+import quevedo.soares.leandro.blemadeeasy.models.BLEDevice
+import java.util.*
+
+class BluetoothDiscoveryAdapter(val inflater: LayoutInflater, val devices: LinkedList<BLEDevice>) :
+    RecyclerView.Adapter<BluetoothDiscoveryAdapter.VH>() {
+
+
+    class VH(item: View) : RecyclerView.ViewHolder(item) {
+        fun bind(device: BLEDevice) {
+            itemView.name.text = device.device?.name
+            itemView.uid.text = device.device.address
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH = VH(
+        inflater.inflate(R.layout.item_bluetooth_device, parent, false)
+    )
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        holder.bind(devices[position])
+    }
+
+    override fun getItemCount(): Int = devices.size
+
+}
