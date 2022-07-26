01ac75106970794263273b7b184bf0e5367842da
Lenna <lennart1705@gmail.com>
2022-07-26 14:25:15 +0200

    added gimmickSystem

--- app/src/main/java/leko/valmx/uhrenprojekt/developertools/DeveloperActivity.kt
+++ app/src/main/java/leko/valmx/uhrenprojekt/developertools/DeveloperActivity.kt
@@ -5,25 +5,40 @@ import android.content.SharedPreferences
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.View
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 import kotlinx.android.synthetic.main.activity_developer.*
 import leko.valmx.uhrenprojekt.R
+import java.lang.reflect.Type
 
 class DeveloperActivity : AppCompatActivity() {
 
-    private val ERROR: Int = -1
-    private val SUCCESS: Int = 0
-    private val INFORMATION: Int = 1
+    private val ERROR: String = "ERROR"
+    private val SUCCESS: String = "SUCCESS"
+    private val INFORMATION: String = "INFO"
+    lateinit var console: ArrayList<Array<String>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_developer)
 
         val prefs = getSharedPreferences("Wort-Uhr", MODE_PRIVATE)
-        val currOutput = prefs.getString("developertools_output", "")
-        developertools_reply.text = currOutput
+        val firstOpen = prefs.getBoolean("developertools_first_open", true)
 
+        if(firstOpen){
+            console = ArrayList<Array<String>>()
+            saveArrayList(console, "console")
+            prefs.edit().putBoolean("developertools_first_open", false).apply()
+        }
+        else{
+            console = getArrayList("console")
+        }
+        console_recycler.adapter = ConsoleAdapter(console)
+        console_recycler.layoutManager = LinearLayoutManager(this)
     }
 
     fun sendMessage(msg: String){
@@ -42,8 +57,8 @@ class DeveloperActivity : AppCompatActivity() {
     fun sendExecutionCommand(view: View){
         val msg = developertools_field_send_msg.text.toString()
         if(msg != "") {
-            showReply("send message: \'$msg\'\n" +
-                    "waiting for response...", SUCCESS)
+            showReply("send message: \'$msg\'", SUCCESS)
+            showReply("waiting for response", SUCCESS)
             sendMessage(msg)
         }
         else{
@@ -51,33 +66,42 @@ class DeveloperActivity : AppCompatActivity() {
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    private fun showReply(pReply: String, success: Int){
-        val prefs = getSharedPreferences("Wort-Uhr", MODE_PRIVATE)
-        val currOutput = prefs.getString("developertools_output", "")
-        var reply = pReply
-        if(success == ERROR){
-            reply = "ERROR: $reply"
+    @SuppressLint("NotifyDataSetChanged")
+    fun showReply(pMsg: String, type: String){
+        var msg = pMsg
+        if(type == INFORMATION){
+            msg = "INFO: $msg"
         }
-        if(success == SUCCESS){
+        if(type == ERROR){
+            msg = "ERROR: $msg"
         }
-        if(success == INFORMATION){
-            reply = "INFO: $reply"
-        }
-        var newOutput = currOutput
-        if(newOutput != ""){
-            newOutput = newOutput + "\n" + reply
-        }
-        else{
-            newOutput = reply
-        }
-        developertools_reply.text = newOutput
-        prefs.edit().putString("developertools_output", newOutput).apply()
+        console.add(arrayOf(msg, type))
+        console_recycler.adapter?.notifyDataSetChanged()
+        saveArrayList(console, "console")
+        console_recycler.smoothScrollToPosition(console.size-1)
     }
 
+    @SuppressLint("NotifyDataSetChanged")
     fun clearConsole(view: View){
-        val prefs = getSharedPreferences("Wort-Uhr", MODE_PRIVATE)
-        prefs.edit().putString("developertools_output", "").apply()
-        developertools_reply.text = ""
+        console.clear()
+        console_recycler.adapter?.notifyDataSetChanged()
+        saveArrayList(console, "console")
+    }
+
+    fun saveArrayList(list: java.util.ArrayList<Array<String>>?, key: String?) {
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor: SharedPreferences.Editor = prefs.edit()
+        val gson = Gson()
+        val json: String = gson.toJson(list)
+        editor.putString(key, json)
+        editor.apply()
+    }
+
+    fun getArrayList(key: String?): ArrayList<Array<String>> {
+        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
+        val gson = Gson()
+        val json: String? = prefs.getString(key, null)
+        val type: Type = object : TypeToken<ArrayList<Array<String>>>() {}.getType()
+        return gson.fromJson(json, type)
     }
 }
