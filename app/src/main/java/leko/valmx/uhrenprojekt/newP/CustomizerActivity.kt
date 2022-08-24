35445b5d09952a69d9461a348327069ab8c7145c
Lenna <lennart1705@gmail.com>
2022-08-24 17:38:33 +0200

    added gimmickSystem

--- app/src/main/java/leko/valmx/uhrenprojekt/newP/CustomizerActivity.kt
+++ app/src/main/java/leko/valmx/uhrenprojekt/newP/CustomizerActivity.kt
@@ -16,6 +16,7 @@ class CustomizerActivity : UhrAppActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customizer)
+        setSupportActionBar(toolbar)
         initRecycler()
     }
 
